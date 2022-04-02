2eebeed7e7d0a65015d459fcecbba6cad71c5de9
brogers5 <6869577+brogers5@users.noreply.github.com>
2022-04-01 23:44:33 -0400

    Remove unused replacement tokens, make source consistent with latest published package version (1.95)

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
-﻿$packageName = '{{PackageName}}'
-$url = '{{DownloadUrl}}'
-$checksum = '{{Checksum}}'
+﻿$packageName = 'multimonitortool'
+$url = 'http://www.nirsoft.net/utils/multimonitortool.zip'
+$checksum = '5c448b256a11c6e2280b05626ebe8cc9fa67307895d0121260e8b3f379b98fe2'
 $checksumType = 'sha256'
-$url64 = '{{DownloadUrlx64}}'
-$checksum64 = '{{Checksumx64}}'
+$url64 = 'http://www.nirsoft.net/utils/multimonitortool-x64.zip'
+$checksum64 = '128949125987ba5264d7e99f6baa50d2c0a500e88c22c62101db0210da1cba4e'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installFile = Join-Path $toolsDir "$($packageName).exe"
