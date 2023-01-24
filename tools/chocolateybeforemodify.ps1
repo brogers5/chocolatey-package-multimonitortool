b2eddbb9337faead38e5d497096e8a61df34c1ed
brogers5 <6869577+brogers5@users.noreply.github.com>
2023-01-23 20:26:21 -0500

    Add beforeModify script to automatically close MultiMonitorTool during upgrades/uninstalls

---
+++ tools/chocolateybeforemodify.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$softwareName = 'MultiMonitorTool'
+$process = Get-Process -Name $softwareName -ErrorAction SilentlyContinue
+if ($process)
+{
+    Write-Warning "$softwareName is currently running, stopping it to prevent upgrade/uninstall from failing..."
+    Stop-Process -InputObject $process -ErrorAction SilentlyContinue
+
+    Start-Sleep -Seconds 3
+
+    $process = Get-Process -Name $softwareName -ErrorAction SilentlyContinue
+    if ($process)
+    {
+        Write-Warning "$softwareName is still running despite stop request, force stopping it..."
+        Stop-Process -InputObject $process -Force -ErrorAction SilentlyContinue
+    }
+
+    Write-Warning "If upgrading, $softwareName may need to be manually restarted upon completion"
+}
+else
+{
+    Write-Debug "No running $softwareName process instances were found"
+}
